2504504c42be6ae505b7431918bee034015f0172
superomosh94 <omondo94@gmail.com>
2025-12-13 15:56:05 +0300

    refactor: Replace dynamic Node.js server with static `http-server` and pre-generated data.

--- start-server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Portfolio Website Local Server...
-echo.
-echo Your portfolio will open at: http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-python -m http.server 8000
